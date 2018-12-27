13ca05b201c8b80c86007a5372dfe183cf600269
mithilesht <mithilesht@cdac.in>
2018-12-27 15:43:56 +0530

    Passing props and maintaining state

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,11 +5,18 @@ class Board extends Component{
 	constructor(props) {
     	super(props);
     	this.state = {
-      		squares: Array(9).fill(null),
+      		squares: Array(9).fill(5),
     	};
   	}
 	renderSquare(i) {
-	    return <Square value={i} />;
+	    return (
+	    	<Square value={this.state.squares[i]} 
+	    			onClick={()=> this.handleClick(i)}
+	    	/>
+	    );
+	}
+	handleClick(i){
+		console.log(i + 'was clicked')
 	}
   	render(){
   		const status = 'Next player: X';
@@ -17,19 +24,19 @@ class Board extends Component{
   			<div>
   				<div>{status}</div>
 				<div style={{height : '60px'}}>
+  					{this.renderSquare(0)}
   					{this.renderSquare(1)}
   					{this.renderSquare(2)}
-  					{this.renderSquare(3)}
   				</div>  				
   				<div>
+  					{this.renderSquare(3)}
   					{this.renderSquare(4)}
   					{this.renderSquare(5)}
-  					{this.renderSquare(6)}
   				</div>
   				<div>
+  					{this.renderSquare(6)}
   					{this.renderSquare(7)}
   					{this.renderSquare(8)}
-  					{this.renderSquare(9)}
   				</div>
   			</div>
   		);
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
 import '../css/Square.css'
 class Square extends Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			value : null,
-		};
-	}
 	render(){
 		return(
 			<button style={{width: '60px', height : '60px',
 							backgroundColor : 'lightgreen'}}
-					onClick={() => this.setState({value : 'X'})}
+					onClick={() => this.props.onClick(this.props.value)}
 			>
-				{this.state.value}
+				{this.props.value}
 			</button>
 		);
 	}
